d6348fa214677c065ddcde4d2b968c00f18d5869
zeshanashraf41 <zeshan@goally.co>
2026-08-17 15:17:20 +0500

    feat: use Dr Umair Arshad portrait

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import { createFileRoute, Link } from "@tanstack/react-router";
 import { ArrowRight, CalendarCheck, HeartPulse, ShieldCheck } from "lucide-react";
 import { useSiteData } from "@/lib/site-data";
 import { SocialLinks } from "@/components/site/SocialLinks";
-import heroImage from "@/assets/pediatric-hero.jpg";
+import heroImage from "@/assets/dr-umair-arshad.png";
 
 
 export const Route = createFileRoute("/")({
